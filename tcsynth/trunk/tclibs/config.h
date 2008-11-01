898106d65229f88021dfee2b7ed3afdf7a3cb946
fromani <fromani@71fa74f9-8357-0410-9cd5-d3b6757aebd8>
2008-11-01 08:08:57 +0000

    add static config.h for tclibs

---
+++ tcsynth/trunk/tclibs/config.h
@@ -0,0 +1,12 @@
+#ifndef TCSYNTH_CONFIG_H
+#define TCSYNTH_CONFIG_H
+
+#ifndef PACKAGE
+#define PACKAGE "tcsynth"
+#endif
+
+#ifndef VERSION
+#define VERSION "0.0.1"
+#endif
+
+#endif /* TCSYNTH_CONFIG_H */
