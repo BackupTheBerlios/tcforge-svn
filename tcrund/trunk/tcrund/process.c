e2383004b423c6c785f294c6f0ce19fb92d7ee1e
fromani <fromani@71fa74f9-8357-0410-9cd5-d3b6757aebd8>
2008-11-04 22:02:14 +0000

    function stubs.

--- tcrund/trunk/tcrund/process.c
+++ tcrund/trunk/tcrund/process.c
@@ -18,8 +18,45 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "libtcutil/tcutil.h"
+
 #include "process.h"
 
+
+struct tcrprocess_ {
+    ;
+};
+
+TCRProcess *tcr_process_new_from_args(char *args[])
+{
+    return NULL;
+}
+
+TCRProcess *tcr_process_new_from_conf(const char *cfgfile)
+{
+    return NULL;
+}
+
+int tcr_process_run(TCRProcess *proc)
+{
+    return TC_ERROR;
+}
+
+int tcr_process_send_command(TCRProcess *proc, TCRCommandCode cmd,
+                             char *args[], char *outbuf)
+{
+    return TC_ERROR;
+}
+
+int tcr_process_status(TCRProcess *proc)
+{
+    return TC_ERROR;
+}
+
+int tcr_process_del(TCRProcess *proc)
+{
+    return TC_ERROR;
+}
 
 
 
